a16619383c8d9ebf4555a6dd7060ef37d094c263
AzizJabbar <azizjabbar1412@gmail.com>
2023-02-18 21:16:35 +0700

    add env variable for api endpoint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: { API_ENDPOINT: "https://gorest.co.in/public/v2" },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
