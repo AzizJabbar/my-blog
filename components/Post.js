0d051528911e79d933da288836c899e8f12791ce
AzizJabbar <azizjabbar1412@gmail.com>
2023-02-22 16:30:17 +0700

    add styling to user detail page

--- components/Post.js
+++ components/Post.js
@@ -2,8 +2,10 @@ export default function Post(props) {
   const { title, body, author } = props;
 
   return (
-    <div>
-      <h2>{title}</h2>
+    <div className=" border p-4 mb-4 w-4/5 bg-white rounded-md h-40 shadow-md hover:cursor-pointer hover:bg-gray-100">
+      <h2 className="text-2xl font-bold mb-2">{title}</h2>
+      <p className="text-gray-600">Author: {author}</p>
+      <p className="text-gray-600">{body.slice(0, 100)}...</p>
     </div>
   );
 }
