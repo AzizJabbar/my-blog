820969627cc7a22a13d038a7085dfb4bfa72a6e0
AzizJabbar <azizjabbar1412@gmail.com>
2023-02-23 11:25:10 +0700

    responsive comment component

--- components/Comment.js
+++ components/Comment.js
@@ -1,11 +1,11 @@
 export default function Comment(props) {
   const { body, name } = props;
   return (
-    <div className="flex my-7 space-x-4">
-      <img src="/favicon.ico" className="w-12" />
+    <div className="flex my-7 space-x-4 items-center">
+      <img src="/favicon.ico" className="md:w-12 md:h-12 h-9 w-9 " />
       <div className="flex flex-col">
         <p className="font-bold">{name}</p>
-        <p className="text-md">{body}</p>
+        <p className="text-md md:text-md text-sm">{body}</p>
       </div>
     </div>
   );
