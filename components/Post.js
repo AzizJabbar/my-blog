5ff52f178cf646a91f9bbc65dc9167f6c3f52e67
AzizJabbar <azizjabbar1412@gmail.com>
2023-02-23 15:54:32 +0700

    fix post item height to fit content

--- components/Post.js
+++ components/Post.js
@@ -7,7 +7,7 @@ export default function Post(props) {
   }
 
   return (
-    <div onClick={() => redirect(id)} className="p-2 border md:p-4 mb-4 w-4/5 bg-white rounded-md h-44 shadow-md hover:cursor-pointer hover:bg-gray-100">
+    <div onClick={() => redirect(id)} className="p-2 border md:p-4 mb-4 w-4/5 bg-white rounded-md min-h-44 h-max shadow-md hover:cursor-pointer hover:bg-gray-100">
       <h2 className="md:text-2xl font-bold mb-2">{title}</h2>
       <p className="text-gray-600 text-sm">Author: {author}</p>
       <p className="text-gray-600 text-sm md:text-md">{body.slice(0, 100)}...</p>
