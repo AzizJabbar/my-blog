ffa5ba9b8a1fdb05be4aacb03aed2d4d3d9d06e6
AzizJabbar <azizjabbar1412@gmail.com>
2023-02-23 11:54:06 +0700

    add styling to update form

--- components/UpdateUserModal.js
+++ components/UpdateUserModal.js
@@ -66,39 +66,47 @@ export default function UpdateUserModal(props) {
             <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
               <form onSubmit={submitUser}>
                 <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                  <div className="sm:flex sm:items-start">
-                    {/* <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <svg className="h-6 w-6 text-green-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
-                      </svg>
-                    </div> */}
-
-                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                      <h3 className="text-lg leading-6 font-medium text-gray-900">Update User</h3>
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3 className="text-lg leading-6 font-medium text-gray-900">Add User</h3>
+                    <div className="mt-3">
+                      <div className="w-full">
+                        <label className="text-sm"> Name </label>
+                        <input
+                          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                          type="text"
+                          name="name"
+                          defaultValue={user.name}
+                          onChange={(event) => setUser((user) => ({ ...user, [event.target.name]: event.target.value }))}
+                          required
+                        />
+                      </div>
                       <div className="mt-2">
-                        <div>
-                          <div>
-                            <label> Name </label>
-                            <input name="name" type="text" defaultValue={user.name} onChange={(event) => setUser((user) => ({ ...user, [event.target.name]: event.target.value }))} required />
-                          </div>
-                          <div>
-                            <label> Email </label>
-                            <input name="email" type="email" defaultValue={user.email} onChange={(event) => setUser((user) => ({ ...user, [event.target.name]: event.target.value }))} required />
-                          </div>
-                          <div>
-                            <label> Gender </label>
-                            <div onChange={(event) => setUser((user) => ({ ...user, [event.target.name]: event.target.value }))}>
-                              <input type="radio" value="male" checked={user.gender === "male"} name="gender" /> Male
-                              <input type="radio" value="female" checked={user.gender === "female"} name="gender" /> Female
-                            </div>
-                          </div>
-                          <div>
-                            <label> Status </label>
-                            <div onChange={(event) => setUser((user) => ({ ...user, [event.target.name]: event.target.value }))}>
-                              <input type="radio" value="active" checked={user.status === "active"} name="status" /> Active
-                              <input type="radio" value="inactive" checked={user.status === "inactive"} name="status" /> Inactive
-                            </div>
-                          </div>
+                        <label className="text-sm"> Email </label>
+                        <input
+                          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                          type="email"
+                          name="email"
+                          defaultValue={user.email}
+                          onChange={(event) => setUser((user) => ({ ...user, [event.target.name]: event.target.value }))}
+                          required
+                        />
+                      </div>
+                      <div className="mt-2">
+                        <label className="text-sm"> Gender </label>
+                        <div className="flex space-x-2 justify-center md:justify-start" onChange={(event) => setUser((user) => ({ ...user, [event.target.name]: event.target.value }))}>
+                          <input type="radio" value="male" name="gender" checked={user.gender === "male"} />
+                          <label>Male</label>
+                          <input type="radio" value="female" name="gender" checked={user.gender === "female"} />
+                          <label>Female</label>
+                        </div>
+                      </div>
+                      <div className="mt-2">
+                        <label className="text-sm"> Status </label>
+                        <div className="flex space-x-2 justify-center md:justify-start" defaultValue={user.name} onChange={(event) => setUser((user) => ({ ...user, [event.target.name]: event.target.value }))}>
+                          <input type="radio" value="active" name="status" checked={user.status === "active"} />
+                          <label>Active</label>
+                          <input type="radio" value="inactive" name="status" checked={user.status === "inactive"} />
+                          <label>Inactive</label>
                         </div>
                       </div>
                     </div>
@@ -110,7 +118,7 @@ export default function UpdateUserModal(props) {
                     type="submit"
                     className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
                   >
-                    OK
+                    Submit
                   </button>
 
                   <button
